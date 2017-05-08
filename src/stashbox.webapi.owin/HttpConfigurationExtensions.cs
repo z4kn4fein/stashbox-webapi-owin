5a5ab6e8fc4ed19612235ee10e94ea2257f44663
Peter Csajtai <peter.csajtai@logmein.com>
2017-05-08 18:53:17 +0200

    Updated webapi package

--- src/stashbox.webapi.owin/HttpConfigurationExtensions.cs
+++ src/stashbox.webapi.owin/HttpConfigurationExtensions.cs
@@ -19,21 +19,47 @@ namespace System.Web.Http
 
         /// <summary>
         /// Sets a custom messagehandler that uses the owin per request scope configured by <see cref="AppBuilderExtensions.UseStashbox"/>.
+        /// <para />
+        /// Configures the <see cref="IStashboxContainer"/> by it's <see cref="StashboxContainerExtensions.AddWebApi"/> extension method.
         /// </summary>
         /// <param name="builder">The app builder.</param>
         /// <param name="configuration">The configuration.</param>
         /// <param name="container">The container.</param>
         /// <exception cref="ArgumentNullException">
-        /// If <paramref name="configuration"/> is <c>null</c>.
+        /// If <paramref name="builder"/> or <paramref name="configuration"/> or <paramref name="container"/> is <c>null</c>.
         /// </exception>
+        /// <seealso cref="AppBuilderExtensions.UseStashbox"/>
+        /// <seealso cref="StashboxContainerExtensions.AddWebApi"/>
         public static IAppBuilder UseStashboxWebApi(this IAppBuilder builder, HttpConfiguration configuration, IStashboxContainer container)
         {
+            Shield.EnsureNotNull(builder, nameof(builder));
             Shield.EnsureNotNull(configuration, nameof(configuration));
+            Shield.EnsureNotNull(container, nameof(container));
 
             if (!configuration.MessageHandlers.Contains(StashboxScopeHandler))
                 configuration.MessageHandlers.Insert(0, StashboxScopeHandler);
 
-            configuration.UseStashbox(container);
+            container.AddWebApi(configuration);
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Sets a custom messagehandler that uses the owin per request scope configured by <see cref="AppBuilderExtensions.UseStashbox"/>.
+        /// </summary>
+        /// <param name="builder">The app builder.</param>
+        /// <param name="configuration">The configuration.</param>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="builder"/> or <paramref name="configuration"/> is <c>null</c>.
+        /// </exception>
+        /// <seealso cref="AppBuilderExtensions.UseStashbox"/>
+        public static IAppBuilder UseStashboxWebApiScopeHandler(this IAppBuilder builder, HttpConfiguration configuration)
+        {
+            Shield.EnsureNotNull(builder, nameof(builder));
+            Shield.EnsureNotNull(configuration, nameof(configuration));
+
+            if (!configuration.MessageHandlers.Contains(StashboxScopeHandler))
+                configuration.MessageHandlers.Insert(0, StashboxScopeHandler);
 
             return builder;
         }
