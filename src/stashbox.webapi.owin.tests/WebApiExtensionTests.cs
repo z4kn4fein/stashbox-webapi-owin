0a0230ec1959becfb9410e1256b126f09cb16516
Peter Csajtai <peter.csajtai@logmein.com>
2017-05-08 02:11:43 +0200

    Added project

---
+++ src/stashbox.webapi.owin.tests/WebApiExtensionTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Owin;
+
+namespace Stashbox.AspNet.WebApi.Owin.Tests
+{
+    [TestClass]
+    public class WebApiExtensionTests
+    {
+        [TestMethod]
+        public async Task WebApiExtensionTests_Scoping()
+        {
+            using (var server = TestServer.Create(app =>
+             {
+                 var config = new HttpConfiguration { IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always };
+                 var container = new StashboxContainer();
+                 container.RegisterScoped<Test>();
+
+                 config.MapHttpAttributeRoutes();
+
+                 app.UseStashboxWebApi(config, container).UseStashbox(container).UseWebApi(config);
+             }))
+            {
+                var resp = await server.HttpClient.GetAsync("/api/test/value");
+                Assert.AreEqual("\"1test1test1\"", await resp.Content.ReadAsStringAsync());
+
+                resp = await server.HttpClient.GetAsync("/api/test/value");
+                Assert.AreEqual("\"2test2test2\"", await resp.Content.ReadAsStringAsync());
+            }
+        }
+    }
+
+    public class Test
+    {
+        private static int counter;
+
+        public Test()
+        {
+            Interlocked.Increment(ref counter);
+        }
+
+        public string Value => "test" + counter;
+    }
+
+    [RoutePrefix("api/test")]
+    public class Test2Controller : ApiController
+    {
+        private readonly Test test;
+        private readonly Test test1;
+
+        private static int controllerCounter;
+
+        public Test2Controller(Test test, Test test1)
+        {
+            this.test = test;
+            this.test1 = test1;
+            Interlocked.Increment(ref controllerCounter);
+        }
+
+        [HttpGet]
+        [Route("value")]
+        public IHttpActionResult GetValue()
+        {
+            return this.Ok(controllerCounter + this.test.Value + this.test1.Value);
+        }
+    }
+}
---
+++ src/stashbox.webapi.owin/HttpConfigurationExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Hosting;
+using Microsoft.Owin;
+using Owin;
+using Stashbox.Infrastructure;
+using Stashbox.Utils;
+using Stashbox.Web.WebApi;
+
+namespace System.Web.Http
+{
+    /// <summary>
+    /// Represents the owin web api related stashbox extensions.
+    /// </summary>
+    public static class AppBuilderOwinWebApiExtensions
+    {
+        private static readonly DelegatingHandler StashboxScopeHandler = new ScopeHandler();
+
+        /// <summary>
+        /// Sets a custom messagehandler that uses the owin per request scope configured by <see cref="AppBuilderExtensions.UseStashbox"/>.
+        /// </summary>
+        /// <param name="builder">The app builder.</param>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="container">The container.</param>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="configuration"/> is <c>null</c>.
+        /// </exception>
+        public static IAppBuilder UseStashboxWebApi(this IAppBuilder builder, HttpConfiguration configuration, IStashboxContainer container)
+        {
+            Shield.EnsureNotNull(configuration, nameof(configuration));
+
+            if (!configuration.MessageHandlers.Contains(StashboxScopeHandler))
+                configuration.MessageHandlers.Insert(0, StashboxScopeHandler);
+
+            configuration.UseStashbox(container);
+
+            return builder;
+        }
+
+        private class ScopeHandler : DelegatingHandler
+        {
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                var context = request.GetOwinContext();
+                var scope = context?.GetCurrentStashboxScope();
+                if (scope != null)
+                    request.Properties[HttpPropertyKeys.DependencyScope] = new StashboxDependencyScope(scope);
+
+                return base.SendAsync(request, cancellationToken);
+            }
+        }
+    }
+}
